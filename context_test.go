bf85063f19289144e3a3b91654e68025405dd042
fahmiaz411 <fahmiaziz884@gmail.com>
2022-03-09 19:24:26 +0700

    first

---
+++ context_test.go
@@ -0,0 +1,15 @@
+package go_context
+
+import (
+	"context"
+	"fmt"
+	"testing"
+)
+
+func TestContext(t *testing.T){
+	background := context.Background()
+	fmt.Println(background)
+
+	todo := context.TODO()
+	fmt.Println(todo)
+}
